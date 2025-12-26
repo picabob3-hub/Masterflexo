e71184ee0ace1203f3cf872121e49a8ff82f1892
picabob3-hub <picabob3@gmail.com>
2025-12-25 21:46:32 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'masterflexo-v1';
+const ASSETS = [
+  'index.html',
+  'manifest.json'
+];
+
+// Instalar el Service Worker y cachear archivos base
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Activar y limpiar versiones antiguas de caché
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Estrategia: Cargar desde caché, si no existe, ir a la red
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      return res || fetch(e.request);
+    })
+  );
+});
